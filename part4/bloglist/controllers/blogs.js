b4fe3889091929fe71cc2720a391d06a00de4ff0
Mert Ilginoglu <mertilginoglu@sabanciuniv.edu>
2023-04-08 20:47:46 +0300

    4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -32,7 +32,21 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  const blog = await Blog.findById(request.params.id)
+
+  if (!user || blog.user.toString() !== user._id.toString())  {
+    return response.status(401).json({error: 'Wrong user'})
+  }
+  user.blogs = user.blogs.filter(b => b.toString() !== blog.id.toString() )
+  await user.save()
+  await blog.remove()
   response.status(204).end()
 })
 
